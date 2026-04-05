cf506313e06d19a7bfcf5e0213b1ff8060a9bd3e
matrizz <henriqueluizsoarestop10@gmail.com>
2026-04-04 22:28:42 -0300

    improve start.sh steps

--- start.sh
+++ start.sh
@@ -2,9 +2,19 @@ npm install
 
 if [ -d "auth" ]; then
   
+  echo ''
   echo "Pasta 'auth' já existe. Iniciando.."
+  echo ''  
+
 else
   echo "Pasta 'auth' ainda não existe. Criando uma.."
+  echo ''  
+  echo "iniciando cold start, prepare-se para escanear o QR Code.."
+  echo ''  
+  echo "Aperte Ctrl + C após escanear e rode 'sh start.sh'."  
+  
+  sh cold-start.sh
+  
   mkdir auth
 fi
 
